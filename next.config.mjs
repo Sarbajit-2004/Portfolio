14d0cf5105fce2acf9b6912691a536e76b0f4713
Sarbajit Kumar De <sarbajit.de.2004@gmail.com>
2025-10-19 13:26:55 +0530

    Install critters for Next optimizeCss

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { optimizeCss: true },
-  images: { remotePatterns: [{protocol:'https',hostname:'**'},{protocol:'http',hostname:'**'}] }
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' }
+    ]
+  }
 };
+
 export default nextConfig;
